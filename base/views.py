58b80e5193a1f487cebdc1b93da38c74a6f6bc0e
Doloris Alas <maestitia0@gmail.com>
2025-11-30 17:04:57 +0300

    second commit

---
+++ base/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'base/home.html')
+
+def posts(request):
+    return render(request, 'base/post.html')
+
+def post(request):
+    return render(request, 'base/posts.html')
+
+
+def profile(request):
+    return render(request, 'base/profile.html')
+
